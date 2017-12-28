7ab0054c5a0b830b3ee930e7fccd7d6c9b1b9ee2
ahmed fadhil <fadhil@fbk.eu>
2017-12-28 18:36:25 +0100

    added buttons and text

--- app/src/main/java/com/example/android/braingame/MainActivity.java
+++ app/src/main/java/com/example/android/braingame/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.braingame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+
+    public void show(View view) {
+        textView.setVisibility(view.VISIBLE);
+    }
+
+    public void hide(View view) {
+        textView.setVisibility(view.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
     }
 }
