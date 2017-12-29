3c6211f06c3710593c5f87bc2f51532bfc86ea80
ahmed fadhil <fadhil@fbk.eu>
2017-12-29 11:37:54 +0100

    created the UI view

--- app/src/main/java/com/example/android/braingame/MainActivity.java
+++ app/src/main/java/com/example/android/braingame/MainActivity.java
@@ -3,26 +3,53 @@ package com.example.android.braingame;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    Button startButton;
+    ImageView logo;
+    TextView title;
+    TextView sumTextView;
 
 
-    public void show(View view) {
-        textView.setVisibility(view.VISIBLE);
-    }
-
-    public void hide(View view) {
-        textView.setVisibility(view.INVISIBLE);
+    public void start(View view) {
+        startButton.setVisibility(View.INVISIBLE);
+        logo.setVisibility(View.INVISIBLE);
+        title.setVisibility(View.INVISIBLE);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.startButton);
+        logo = (ImageView) findViewById(R.id.logo);
+        title = (TextView) findViewById(R.id.title);
+        sumTextView = (TextView) findViewById(R.id.sumTextView);
+        Random randomNumber = new Random();
 
-        textView = (TextView) findViewById(R.id.textView);
+        int a = randomNumber.nextInt(21);
+        int b = randomNumber.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + "+" + Integer.toString(b));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
